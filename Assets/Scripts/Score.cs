6b421eeb88dfd2117aa6fa636f17f937318cb2ba
Luís Dutra <88776444+luisFHDutra@users.noreply.github.com>
2024-10-06 09:40:36 -0300

    Project files

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public TextMeshProUGUI scoreText;
+
+    void Update()
+    {
+        scoreText.text = player.position.z.ToString("0");
+    }
+}
